e01d31f0fb13ba3e84907812abddcfb322be57e6
Pedro Gutierrez <pedrogutierreznavarro@gmail.com>
2022-08-28 22:47:44 -0600

    Add Material UI table and header component

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { Snackbar } from './snackbar/Snackbar';
 export { CurrenciesTable } from './currencies-table/CurrenciesTable';
+export { Header } from './header/Header';
--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,6 @@
 export const fetchCurrencies = (page, currenciesPerPage = 10) => {
-  return fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${currenciesPerPage}&page=${page}&sparkline=false`)
+  // return fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${currenciesPerPage}&page=${page}&sparkline=false`)
+  return fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&sparkline=false`)
       .then(res => res.json())
       .then(data => {
         if (data.error) {
